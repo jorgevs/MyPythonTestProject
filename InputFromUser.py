98080a54e89972d6a044cd1d036f4e93e57a0de8
jorgevs <jevzsz@gmail.com>
2016-07-21 22:28:35 -0400

    Adding more and more new stuff

--- InputFromUser.py
+++ InputFromUser.py
@@ -1,12 +1,19 @@
-# Getting input from the user =======================================================
+from sys import exit
+
+# Getting input from the user =========================================================================
 response = raw_input("Please enter your name: ")
 print'Hello ' + response
+if 'Jorge' in response or 'Luis' in response:
+    print "Wonderful name! =)   You win!"
+    exit(0)
 # =====================================================================================================
 
 response = raw_input("male of female?")
 while response != "male":
     response = raw_input("male of female?")
-print("Great answer!!!")  # =====================================================================================================
+print("Great answer!!!")
+
+# =====================================================================================================
 
 # Here is an example Python command line script that demonstrates how to obtain data from the user with
 # the Python 2 or 3 interpreter.
