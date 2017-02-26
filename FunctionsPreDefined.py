a40ddc77f06f63f5b56dfaad2df0c27168ad92a3
JorgeVS <jevzsz@gmail.com>
2017-02-26 11:03:57 -0500

    Adding some separation lines

---
+++ FunctionsPreDefined.py
@@ -0,0 +1,10 @@
+print abs(-50)
+
+print bool(11)
+print bool(0)
+print bool("ddd")
+print bool("")
+print bool([])
+
+#dir function indicates the funtions that can be used with the specified object
+print dir([])
