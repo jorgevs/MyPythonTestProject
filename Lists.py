46d94c86a6b0a03f8c62e405446f5b72b9341f45
Jorge Vazquez <jvazquez@forrester.com>
2018-10-10 20:07:04 -0400

    Adding more files and support to python3

--- Lists.py
+++ Lists.py
@@ -1,33 +1,33 @@
 # Lists ==============================================================================
-print "================================================================================"
+print ("================================================================================")
 
 dynamic_languages = ['Python', 'Ruby', 'Groovy']
 dynamic_languages.append('Lisp')
-print "dynamic_languages is: %r" % dynamic_languages
+print ("dynamic_languages is: %r" % dynamic_languages)
 
-print "================================================================================"
+print ("================================================================================")
 
 myList = ['Jorge', 'Silvia', 'Magda', 'Eto', 'Michael']
 myList.insert(2, 'Didier')
 myList[5] = 'Igue'
-print "myList is: %r" % myList
-print "myList sorted: %r" % sorted(myList)
+print ("myList is: %r" % myList)
+print ("myList sorted: %r" % sorted(myList))
 
-print 'myList[0]: %s' % myList[0]
-print 'myList[4]: %s' % myList[4]
-print 'myList[1:4]: %s' % myList[1:4]
-print "max: %s" % max(myList)
-print "min: %s" % min(myList)
-print "Is Jorge in myList?: %s" % ("Jorge" in myList)
-print "Index of Silvia in myList: %d" % myList.index("Silvia")
+print ('myList[0]: %s' % myList[0])
+print ('myList[4]: %s' % myList[4])
+print ('myList[1:4]: %s' % myList[1:4])
+print ("max: %s" % max(myList))
+print ("min: %s" % min(myList))
+print ("Is Jorge in myList?: %s" % ("Jorge" in myList))
+print ("Index of Silvia in myList: %d" % myList.index("Silvia"))
 
-print "================================================================================"
+print ("================================================================================")
 animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
-print "Iterating through animals:"
+print ("Iterating through animals:")
 for name in animals:
-    print ">> " + name
+    print (">> " + name)
 
-print "================================================================================"
+print ("================================================================================")
 
 otraLista = [1, 2, 4, 5, 9]
 otraLista.append(6)
@@ -35,12 +35,12 @@ otraLista.append(7)
 otraLista.remove(7)
 otraLista.insert(2, 3)
 otraLista.reverse()
-print "otraLista is: %r" % otraLista
+print ("otraLista is: %r" % otraLista)
 print("len: ", len(otraLista))
 print("max: ", max(otraLista))
 print("min: ", min(otraLista))
 
-print "================================================================================"
+print ("================================================================================")
 
 for number in [10, 20, 30, 40, 50, 60]:
     print(number)
@@ -48,20 +48,20 @@ for number in [10, 20, 30, 40, 50, 60]:
 for d in range(0, 10):
     print(d)
 
-print "================================================================================"
+print ("================================================================================")
 
 num_list = [[1, 2, 3], [10, 20, 30], [100, 200, 300]]
 for a in range(0, 3):
     for b in range(0, 3):
         print(num_list[a], [b])
 
-print "================================================================================"
+print ("================================================================================")
 
 listaDeListas = [myList, otraLista]
-print "listaDeListas es: %r" % listaDeListas
+print ("listaDeListas es: %r" % listaDeListas)
 
-print "================================================================================"
+print ("================================================================================")
 # A Python way of copying a list. You use the list slice syntax [:] to
 # effectively make a slice from the very first element to the very last one.
 newList = myList[:]
-print newList
+print (newList)
--- MainParameters.py
+++ MainParameters.py
@@ -1,23 +1,23 @@
 import sys
 
 def myFuntion(param):
-    print "This is my function"
+    print ("This is my function")
 
     try:
-        print sys.argv[1]
+        print (sys.argv[1])
     except IndexError:
-        print "IndexError"
+        print ("IndexError")
     finally:
-        print "Finally"
+        print ("Finally")
 
     return "The received parameter is: " + param
 
 
-print sys.argv[0]
+print (sys.argv[0])
 if __name__ == "__main__":   # This will only be true if the script is called as the main program
-    print "Hi"
+    print ("Hi")
     x = myFuntion("XX")
-    print x
+    print (x)
 
 
 
