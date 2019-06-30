c0ff823e8fa5a54af4cf08541c48a58ccbe2b245
jorgevs.mx <jorgevs.mx@gmail.com>
2019-06-30 15:59:55 -0400

    Adding samples for pywin32

--- getPixelColor.py
+++ getPixelColor.py
@@ -1,5 +1,10 @@
 import win32.win32api as api
 
+# Documentation:
+# http://timgolden.me.uk/pywin32-docs/win32api.html
+# Github Repo
+# https://github.com/mhammond/pywin32
+
 while True:
     x, y = api.GetCursorPos()
     print("Mouse position: x: %d y: %d" % (x, y))
--- speakLoud.py
+++ speakLoud.py
@@ -1,4 +1,7 @@
 import win32com.client
 
+# Github Repo
+# https://github.com/mhammond/pywin32
+
 speaker = win32com.client.Dispatch("SAPI.SpVoice")
 speaker.Speak("Magda, feed your husband!")
