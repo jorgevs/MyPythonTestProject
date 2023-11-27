72adffd3d2617c9b30b2bb0cd8bd1f28792a57e2
JorgeVS <jvazquez@forrester.com>
2023-11-27 10:48:30 -0500

    improvements on addReposToTeams script

--- GitHub/addReposToTeam.py
+++ GitHub/addReposToTeam.py
@@ -3,27 +3,28 @@ import requests
 # This script adds all existing repositories in the ForresterTM organization into the "devops-integration" team
 # with a reading access level.
 
-token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+token = 'xxxxxxxxxxxxxxx'
 
 items_per_page = 100
 pages = 8
-
-data = '{"permission": "pull"}'
+GitHubTeam = "SecurityTeam"   # Team you want to update
+#data = '{"permission": "pull"}'
+data = '{"permission": "admin"}'  # This could be 'read', 'triage', 'write', 'maintain', 'admin'
 
 
 for page in range(1, pages + 1):
     url = 'https://api.github.com/orgs/ForresterTM/repos?per_page=' + str(items_per_page) + '&page=' + str(page)
     response = requests.get(url, headers={'Accept': 'application/vnd.github+json', 'X-GitHub-Api-Version': '2022-11-28', 'Authorization': 'Bearer ' + token})
-    #print("Status code: ", response.status_code)
+    print("Status code: ", response.status_code)
 
     response_list = response.json()
-    # print(response_list)
+    print(response_list)
 
     for item in response_list:
         print(">> " + item["name"])
         repo = item["name"]
-        # Adds every repo to the 'devops-integration' team
-        url = 'https://api.github.com/orgs/ForresterTM/teams/devops-integration/repos/ForresterTM/' + repo
+        # Adds every repo to the 'GitHubTeam' team
+        url = 'https://api.github.com/orgs/ForresterTM/teams/' + GitHubTeam + '/repos/ForresterTM/' + repo
         response = requests.put(url, data=data, headers={'Accept': 'application/vnd.github+json', 'X-GitHub-Api-Version': '2022-11-28', 'Authorization': 'Bearer ' + token})
         print(repo + " > Status code: ", response.status_code)
         # If Status code = 204 then it means that it got added to the team successfully
